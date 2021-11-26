83f7cb6d4b2b9d16e707e26861d96904b72b2d74
theo-bnts <theo2bont@gmail.com>
2021-11-26 14:39:32 +0100

    feat: moderation of unauthorised attempts at mentions

--- moderation/07-mentions.js
+++ moderation/07-mentions.js
@@ -5,4 +5,8 @@ export async function run (client, message, settings) {
 
     if (message.mentions.users.size + message.mentions.roles.size >= settings.mentions_moderation_count)
         return processDeletion(message, 'nombreuses mentions')
+
+    if (!message.mentions.everyone && (message.content?.includes('@everyone') || message.content?.includes('@here')))
+        return processDeletion(message, '@everyone / @here')
+
 }
